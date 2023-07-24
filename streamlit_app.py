e53856ec4ec9b0573e64bb50a45ee7348da99be5
xarn902 <65642036+xarn902@users.noreply.github.com>
2023-07-24 14:58:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-st.multiselect("Pick some fruits: ", list(my_fruit_list.fruit))
+st.multiselect("Pick some fruits: ", list(my_fruit_list['fruit']))
 
 st.dataframe(my_fruit_list)
 
