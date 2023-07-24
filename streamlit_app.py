ab2c6c8a28cc744c38741b8f72c90d220ba706fe
xarn902 <65642036+xarn902@users.noreply.github.com>
2023-07-24 14:48:26 -0500

    Update streamlit_app.py
    Multiselect added

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,11 @@ st.text('Omega 3 & Blueberry Oatmeal')
 st.text('Kale, Spinach & Rocket Smoothie')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-st.text('mango')
-st.text('Strawberry')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+st.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+
 st.dataframe(my_fruit_list)
+
+
