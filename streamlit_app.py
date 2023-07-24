5b638f2f9f098f7a05d96787c1e96d81d91f13bc
xarn902 <65642036+xarn902@users.noreply.github.com>
2023-07-24 12:54:08 -0500

    Update streamlit_app.py
    New menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
 st.title('My Parents New Healthy Diner')
+st.header('🐔 🥑🍞Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
 
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.text('mango')
+st.text('Strawberry')
