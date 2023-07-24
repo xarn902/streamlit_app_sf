9dddb36869b8a01994e054c89df1703dd5bee2dc
xarn902 <65642036+xarn902@users.noreply.github.com>
2023-07-24 13:02:36 -0500

    Update streamlit_app.py
    Dataframe from bucket added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title('My Parents New Healthy Diner')
 st.header('🐔 🥑🍞Breakfast Menu')
@@ -8,3 +9,6 @@ st.text('Kale, Spinach & Rocket Smoothie')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 st.text('mango')
 st.text('Strawberry')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
