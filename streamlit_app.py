5a5a15c7dd86537404a85e99cc7087b2596815b5
xarn902 <65642036+xarn902@users.noreply.github.com>
2023-07-25 10:53:50 -0500

    Update streamlit_app.py
    Fruits index section commented

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,10 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 selected_fruits = st.multiselect("Pick some fruits: ", list(my_fruit_list['Fruit']))
-print(selected_fruits)
+
+#my_fruit_list = my_fruit_list.set_index('Fruit')
+#st.multiselect("Pick some fruits: ", list(my_fruit_list.index)
+
 
 my_fruit_list_filtered = my_fruit_list[my_fruit_list['Fruit'].isin(selected_fruits)]
 
