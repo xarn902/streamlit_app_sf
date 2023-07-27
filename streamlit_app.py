dcb9b7a2ba935222c890e4a943e1b2fc013d1ef6
xarn902 <65642036+xarn902@users.noreply.github.com>
2023-07-27 18:47:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#st.text(fruityvice_response.json())
 
 
 # write your own comment -what does the next line do? 
